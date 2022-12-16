b394cf3b7f4374566cf13d91112a9dff61313f59
prathibha22 <prccherukuri@gmail.com>
2022-12-16 11:50:18 -0500

    All annotations added and tests passed

--- src/main/java/com/zipcodewilmington/bakery/repositories/BakerRepository.java
+++ src/main/java/com/zipcodewilmington/bakery/repositories/BakerRepository.java
@@ -3,5 +3,8 @@ package com.zipcodewilmington.bakery.repositories;
 import com.zipcodewilmington.bakery.models.Baker;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface BakerRepository extends CrudRepository<Baker, Long> {
+    public Optional<Baker> findById(Long id);
 }
--- src/main/java/com/zipcodewilmington/bakery/repositories/MuffinRepository.java
+++ src/main/java/com/zipcodewilmington/bakery/repositories/MuffinRepository.java
@@ -3,5 +3,8 @@ package com.zipcodewilmington.bakery.repositories;
 import com.zipcodewilmington.bakery.models.Muffin;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface MuffinRepository extends CrudRepository<Muffin, Long> {
+    public Optional<Muffin> findById(Long id);
 }
